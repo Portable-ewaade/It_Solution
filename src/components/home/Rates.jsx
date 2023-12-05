1aeabe54e7e967816894cd600b5a5b314d0963fa
Kehinde Adetule <kehinde.adetule@theacconsulting.com>
2023-12-05 20:49:38 +0100

    review

--- src/components/home/Rates.jsx
+++ src/components/home/Rates.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Col, Row } from "react-bootstrap";
-import styles from '../../styles/Home.module.css'
+import styles from "../../styles/Home.module.css";
 
 const Rates = () => {
   return (
@@ -15,46 +15,46 @@ const Rates = () => {
           <Row className="text-center">
             <Col md={3}>
               <div
-                className="card rounded text-white"
+                className="card text-white"
                 style={{
                   backgroundColor: "#032376",
                 }}
               >
-                <h1>100</h1>
-                <p>Hours Saved Annually</p>
+                <h1 className="fw-bolder fs_large mt-5">100</h1>
+                <p className="fw-medium fs_normal">Projects completed</p>
               </div>
             </Col>
             <Col md={3}>
               <div
-                className="card rounded text-white"
+                className="card text-white"
                 style={{
                   backgroundColor: "#03762A",
                 }}
               >
-                <h1>+10.000</h1>
-                <p>Projects completed</p>
+                <h1 className="fw-bolder fs_large mt-5">+10.000</h1>
+                <p className="fw-medium fs_normal"> Hours Saved Annually</p>
               </div>
             </Col>
             <Col md={3}>
               <div
-                className="card rounded text-white"
+                className="card text-white"
                 style={{
                   backgroundColor: "#3498DB",
                 }}
               >
-                <h1>200K</h1>
-                <p>APR</p>
+                <h1 className="fw-bolder fs_large mt-5">200K</h1>
+                <p className="fw-medium fs_normal">APR</p>
               </div>
             </Col>
             <Col md={3}>
               <div
-                className="card rounded text-white"
+                className="card text-white"
                 style={{
                   backgroundColor: "#660376",
                 }}
               >
-                <h1>250</h1>
-                <p>Unique Users</p>
+                <h1 className="fw-bolder fs_large mt-5">250</h1>
+                <p className="fw-medium fs_normal">Unique Users</p>
               </div>
             </Col>
           </Row>
